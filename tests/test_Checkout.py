1d2b54d92fc67469dfb0bf0d4306b6c99e61cc8a
caioaza <caioaza@gmail.com>
2025-02-16 12:20:35 +0000

    Changes for comment sections

--- tests/test_Checkout.py
+++ tests/test_Checkout.py
@@ -1,9 +1,9 @@
 import json
 import os
 import sys
-
+###adding
 import pytest
-
+###
 # Add the root directory to the path
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
